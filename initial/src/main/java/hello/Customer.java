12eb13ff61c361b90ee8c5376998e3c13ad5681b
Maciej Matys <mmatys@46Q9N32>
2015-06-08 19:23:54 +0200

    Customer POJO

---
+++ initial/src/main/java/hello/Customer.java
@@ -0,0 +1,21 @@
+package hello;
+
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+
+//@ToString
+public class Customer {
+
+    @Id
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
