302b551a01b90b758cda727192b3b9092f806339
Maciej Matys <mmatys@46Q9N32>
2015-06-09 08:05:15 +0200

    REST application

--- initial/src/main/java/hello/CustomerRepository.java
+++ initial/src/main/java/hello/CustomerRepository.java
@@ -1,13 +1,16 @@
 package hello;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+//@RepositoryRestResource
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 
     Customer findByFirstName(String firstName);
 
-    List<Customer> findByLastName(String lastName);
+    List<Customer> findByLastName(@Param("name") String lastName);
 
 }
