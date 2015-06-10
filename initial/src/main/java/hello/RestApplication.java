7223f0914e5e49c3ab2dc978c25faa32675feea7
Maciej Matys <mmatys@46Q9N32>
2015-06-10 10:46:02 +0200

    Added support for externalized configuration.

--- initial/src/main/java/hello/RestApplication.java
+++ initial/src/main/java/hello/RestApplication.java
@@ -2,8 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class RestApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
