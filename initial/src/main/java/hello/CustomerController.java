06d7e6770d1b34aeb3c834ce6be7c841f0b729bf
Maciej Matys <mmatys@46Q9N32>
2015-06-10 10:12:40 +0200

    custom REST controller

---
+++ initial/src/main/java/hello/CustomerController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    @Autowired
+    AppConfig appConfig;
+
+    @RequestMapping("/customer")
+    Customer customer(@RequestParam(value = "name", required = false) String name) {
+        return repository.findByFirstName(name != null ? name : appConfig.getDefaultName());
+    }
+}
