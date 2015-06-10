fea5b36437849befd797e10860ea4f6b53fae57d
Maciej Matys <mmatys@46Q9N32>
2015-06-10 10:13:58 +0200

    Cleanup

--- initial/src/main/java/hello/CustomerRepository.java
+++ initial/src/main/java/hello/CustomerRepository.java
@@ -2,11 +2,9 @@ package hello;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-//@RepositoryRestResource
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 
     Customer findByFirstName(String firstName);
