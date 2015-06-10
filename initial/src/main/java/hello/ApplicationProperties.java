03cfab3510e3ecbc7674ae9cae04fafcf0b73020
Maciej Matys <mmatys@46Q9N32>
2015-06-10 23:20:50 +0200

    Generating your own meta-data using the annotation processor in a better way :)

--- initial/src/main/java/hello/ApplicationProperties.java
+++ initial/src/main/java/hello/ApplicationProperties.java
@@ -4,11 +4,17 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @Component
 @ConfigurationProperties(prefix = "hello")
-public class AppConfig {
+public class ApplicationProperties {
 
+    /**
+     * Description for hello.defaultName
+     */
+    @NotNull
     private String defaultName;
 
 }
