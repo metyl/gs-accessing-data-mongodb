87c50126e15429f2da01cb144cdf105ca18b4880
Maciej Matys <mmatys@46Q9N32>
2015-06-16 13:25:00 +0200

    package app as .war file that is both executable and deployable into an external container but fails in IntelliJ Spring Boot Application Run Config

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -3,10 +3,11 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
